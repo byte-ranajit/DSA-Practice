0448882eec65f2a20be5c81dcaadbc2844d0c84a
byte-ranajit <ranajitkhandual@gmail.com>
2025-12-31 18:37:44 +0530

    no changes

--- src/main/java/org/example/array/ArrayTestOne.java
+++ src/main/java/org/example/array/ArrayTestOne.java
@@ -1,9 +1,29 @@
 package org.example.array;
 
 public class ArrayTestOne {
-    public static void displayArray(int [] arr){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+    public static void displayArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
+    }
+
+    public static Boolean binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                return true;
+            }
+
+            if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
     }
 }
