0388c49d774073389570d38c0d36d6a3b05df5df
byte-ranajit <ranajitkhandual@gmail.com>
2025-12-31 08:35:22 +0530

    display the array

---
+++ src/main/java/org/example/array/ArrayTestOne.java
@@ -0,0 +1,9 @@
+package org.example.array;
+
+public class ArrayTestOne {
+    public static void displayArray(int [] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
