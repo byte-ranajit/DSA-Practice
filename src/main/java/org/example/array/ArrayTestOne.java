466d85353791d71f19c376f399689c505f62bdb2
byte-ranajit <ranajitkhandual@gmail.com>
2026-02-03 12:12:56 +0530

    no changes

--- src/main/java/org/example/array/ArrayTestOne.java
+++ src/main/java/org/example/array/ArrayTestOne.java
@@ -78,7 +78,9 @@ public class ArrayTestOne {
     }
 
     private static int firstCountOccurance(int [] arr, int target){
-        int ans = -1, left = 0, right = arr.length - 1;
+        int ans = -1;
+        int  left = 0;
+        int  right = arr.length - 1;
         while(left <= right){
             int mid = left + (right - left) / 2;
             if(arr[mid] == target){
@@ -94,7 +96,9 @@ public class ArrayTestOne {
     }
 
     private static int lastCountOccurance(int [] arr, int target){
-        int ans = -1, left = 0, right = arr.length - 1;
+        int ans = -1;
+        int  left = 0;
+        int  right = arr.length - 1;
         while(left <= right){
             int mid = left + (right - left) / 2;
             if(arr[mid] == target){
